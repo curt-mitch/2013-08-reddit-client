c5e26d830f8205d410f6b57ea4680a9330dd8f32
Hack Reactor Students <github@hackreactor.com>
2013-09-04 16:39:43 -0700

    first commit - currently showing list of thumbnails from reddit/r/pics

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,11 @@
-angular.module('myApp.controllers', []);
+var redditUrl = 'http://www.reddit.com/r/pics.json';
+
+angular.module('myApp.controllers', [])
+.controller('MainController', function($scope, $http) {
+    $http({
+      method: 'JSONP',
+      url: redditUrl + '?jsonp=JSON_CALLBACK'
+    }).success(function(data, status){
+      $scope.news = data.data.children;
+    });
+});
