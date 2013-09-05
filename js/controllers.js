53f977c4465eb845aa4c58707163eb133d911367
Hack Reactor Students <github@hackreactor.com>
2013-09-04 17:15:22 -0700

    Added up and downvoting

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,20 @@ var redditUrl = 'http://www.reddit.com/r/pics.json';
 
 angular.module('myApp.controllers', [])
 .controller('MainController', function($scope, $http) {
+    $scope.add = function(item) {
+      if (item.hasOwnProperty("counter")) {
+        item.counter++;
+      } else {
+        item.counter = 0;
+      }
+    };
+    $scope.subtract = function(item) {
+      if (item.hasOwnProperty("counter")) {
+        item.counter--;
+      } else {
+        item.counter = 0;
+      }
+    };
     $http({
       method: 'JSONP',
       url: redditUrl + '?jsonp=JSON_CALLBACK'
